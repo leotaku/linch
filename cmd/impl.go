9696fafef9e234f2c1042539ba02f856bd06883e
Leo Gaskin <leo.gaskin@le0.gs>
2021-01-26 21:46:10 +0100

    Commit new implementation

--- cmd/impl.go
+++ cmd/impl.go
@@ -3,56 +3,22 @@ package cmd
 import (
 	"bufio"
 	"errors"
-	"fmt"
 	"net/url"
 	"os"
 	"regexp"
 	"sync"
 	"time"
 
-	"github.com/logrusorgru/aurora"
 	http "github.com/valyala/fasthttp"
 )
 
-var regexpGuessOne = regexp.MustCompile(`(https?|file):\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
-
-type ActionKind int
-
-const (
-	ActionKindOk ActionKind = iota + 1
-	ActionKindRedir
-	ActionKindSemiRedir
-	ActionKindErr
-	ActionKindUrl
-)
+var regexpGuessOne = regexp.MustCompile(`(https?):\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
 
 type Action struct {
-	Kind     ActionKind
 	Original *url.URL
 	Redir    *url.URL
 	Status   int
 	Err      error
-}
-
-var au = aurora.NewAurora(true)
-
-func (a Action) String() string {
-	switch a.Kind {
-	case ActionKindOk:
-		return fmt.Sprintf("SUCCE %v: %v", au.Green(a.Status), a.Original.String())
-	case ActionKindRedir:
-		redir, _ := url.QueryUnescape(a.Redir.String())
-		return fmt.Sprintf("REDIR %v: %v -> %v", au.Yellow(a.Status), a.Original.String(), redir)
-	case ActionKindSemiRedir:
-		redir, _ := url.QueryUnescape(a.Redir.String())
-		return fmt.Sprintf("SEMIR %v: %v -> %v", au.Blue(a.Status), a.Original.String(), redir)
-	case ActionKindErr:
-		return fmt.Sprintf("ERROR %v: %v", au.Red(a.Status), a.Original.String())
-	case ActionKindUrl:
-		return fmt.Sprintf("URLER %v: %v", au.Magenta("XXX"), a.Err)
-	default:
-		panic("unreachable")
-	}
 }
 
 func extractLinksForPaths(s *bufio.Scanner, send chan string) {
@@ -79,18 +45,20 @@ func extractLinksForPath(path string, send chan string) {
 }
 
 func startLinkHandler() (chan string, chan Action) {
-	links := make(chan string, 100)
+	links := make(chan string, 1000)
 	rsps := make(chan Action, 100)
+
 	go func() {
 		wg := new(sync.WaitGroup)
-		for link := range links {
-			// Naive rate limiting
-			time.Sleep(waitArg)
+		for i := 0; i < limitArg; i++ {
 			wg.Add(1)
-			go func(wg *sync.WaitGroup, link string) {
-				handleRawLink(link, rsps)
+			go func(wg *sync.WaitGroup) {
+				for link := range links {
+					handleRawLink(link, rsps)
+					time.Sleep(waitArg)
+				}
 				wg.Done()
-			}(wg, link)
+			}(wg)
 		}
 		wg.Wait()
 		close(rsps)
@@ -109,7 +77,7 @@ func handleRawLink(rawurl string, rsps chan Action) {
 
 	url, err := url.Parse(rawurl)
 	if err != nil {
-		rsps <- Action{Kind: ActionKindUrl, Err: err}
+		rsps <- Action{Err: err}
 	} else {
 		rsps <- handleLink(*url)
 	}
@@ -121,37 +89,45 @@ func handleLink(link url.URL) Action {
 	defer http.ReleaseRequest(req)
 	defer http.ReleaseResponse(resp)
 
+	req.Header.SetMethod("HEAD")
 	req.SetRequestURI(link.String())
-	http.DoTimeout(req, resp, timeoutArg)
+	err := http.DoTimeout(req, resp, timeoutArg)
 	status := resp.StatusCode()
 
-	switch status {
-	case 200:
+	switch {
+	case err != nil:
 		return Action{
-			Kind:     ActionKindOk,
 			Original: &link,
-			Redir:    nil,
-			Status:   status,
+			Err:      err,
 		}
-	case 301, 308, 302, 307:
+	case status >= 300 && status < 400:
 		redir := string(resp.Header.Peek("Location"))
 		redurl, err := url.Parse(redir)
-		if len(redir) != 0 && err == nil {
-			kind := ActionKindRedir
-			if status == 302 || status == 307 {
-				kind = ActionKindSemiRedir
+		if len(redir) == 0 {
+			err = errors.New("missing location in redirection")
+		} else {
+			if len(redurl.Host) == 0 {
+				redurl.Host = link.Host
 			}
+			if len(redurl.Scheme) == 0 {
+				redurl.Scheme = link.Scheme
+			}
+		}
+		if err == nil {
 			return Action{
-				Kind:     kind,
 				Original: &link,
 				Redir:    redurl,
 				Status:   status,
 			}
+		} else {
+			return Action{
+				Original: &link,
+				Err:      err,
+				Status:   status,
+			}
 		}
-		fallthrough
 	default:
 		return Action{
-			Kind:     ActionKindErr,
 			Original: &link,
 			Redir:    nil,
 			Status:   status,
--- cmd/root.go
+++ cmd/root.go
@@ -3,27 +3,24 @@ package cmd
 import (
 	"bufio"
 	"fmt"
+	"net/url"
 	"os"
 	"time"
 
+	"github.com/logrusorgru/aurora/v3"
 	"github.com/spf13/cobra"
 )
 
-var guessArg int
+var limitArg int
 var timeoutArg time.Duration
 var waitArg time.Duration
+var noColorArg bool
 
 var rootCmd = &cobra.Command{
-	Use:   "linch",
-	Short: "Linch is an unixy non-recursive link validator",
-	Long: `Linch is an unixy non-recursive link validator.
-
-It helps you by checking if all links defined in your
-files are still valid.  If they are not, Linch might
-even offer suggestions on how to fix them.
-
-Linch supports http, https and local file links.`,
-	Args: cobra.NoArgs,
+	Use:     "linch [flags..]",
+	Short:   "Linch is a simplistic non-recursive link validator",
+	Version: "0.1",
+	Args:    cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		cmd.SilenceUsage = true
 
@@ -31,12 +28,14 @@ Linch supports http, https and local file links.`,
 		lines := bufio.NewScanner(os.Stdin)
 		go extractLinksForPaths(lines, links)
 
+		au := aurora.NewAurora(!noColorArg)
 		for rsp := range rsps {
-			fmt.Println(rsp)
+			fmt.Println(rsp.Pretty(au))
 		}
 
 		return nil
 	},
+	DisableFlagsInUseLine: true,
 }
 
 func Execute() {
@@ -46,7 +45,29 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().CountVarP(&guessArg, "guess", "g", "guess level when identifying links (0-2)")
-	rootCmd.Flags().DurationVarP(&timeoutArg, "timeout", "t", 3 * time.Second, "timeout for resolving http requests")
-	rootCmd.Flags().DurationVarP(&waitArg, "wait", "w", 0, "wait time between http requests (default 0s)")
+	rootCmd.Flags().IntVarP(&limitArg, "limit", "l", 100, "limit number of concurrent connections")
+	rootCmd.Flags().DurationVarP(&timeoutArg, "timeout", "t", 3*time.Second, "timeout for resolving requests")
+	rootCmd.Flags().DurationVarP(&waitArg, "wait", "w", 0, "wait time between individual requests (default 0s)")
+	_, no_color := os.LookupEnv("NO_COLOR")
+	rootCmd.Flags().BoolVarP(&noColorArg, "no-color", "n", no_color, "whether to disable colors in output")
+	rootCmd.Flags().SortFlags = false
+}
+
+func (a Action) Pretty(au aurora.Aurora) string {
+	switch {
+	case a.Err != nil && a.Status == 0:
+		return fmt.Sprintf("INTER %v: %v", au.Magenta("XXX"), a.Err)
+	case a.Err != nil:
+		return fmt.Sprintf("INTER %v: %v", au.Magenta(a.Status), a.Err)
+	case a.Status < 300:
+		return fmt.Sprintf("SUCCE %v: %v", au.Green(a.Status), a.Original.String())
+	case a.Status == 301 || a.Status == 308:
+		redir, _ := url.QueryUnescape(a.Redir.String())
+		return fmt.Sprintf("REDIR %v: %v -> %v", au.Yellow(a.Status), a.Original.String(), redir)
+	case a.Status == 302 || a.Status == 307:
+		redir, _ := url.QueryUnescape(a.Redir.String())
+		return fmt.Sprintf("SEMIR %v: %v -> %v", au.Blue(a.Status), a.Original.String(), redir)
+	default:
+		return fmt.Sprintf("ERROR %v: %v", au.Red(a.Status), a.Original.String())
+	}
 }
